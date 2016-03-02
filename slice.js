9497a49251ed0639949b3ab12b34d193e56d2cc8
Mikol Graves <mikol@thinbox.org>
2016-03-02 05:31:08 -0800

    Initial implementation.

---
+++ slice.js
@@ -0,0 +1,10 @@
+/* jshint ignore:start *//* jscs:disable */
+!function(e,o){'use strict';var t=[/* DEPENDENCIES */];if('function'==typeof define&&define.amd)define(t,function(){return o.apply(e,[].slice.call(arguments))});else if('object'==typeof module&&module.exports){for(var n=t.length;n--;)t[n]=require(t[n]);module.exports=o.apply(e,t)}}('object'==typeof global&&global||'object'==typeof window&&window||this,
+
+function () {
+/* jshint ignore:end   *//* jscs:enable  */
+  'use strict';
+
+  var prototype = [];
+  return prototype.slice.call.bind(prototype.slice);
+});
